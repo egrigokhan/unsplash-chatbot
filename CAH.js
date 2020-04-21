9c330cd5fd609f9ea174ad5db65db41de96bb66b
Gokhan Egri <egrigokhan@gmail.com>
2020-04-21 23:19:28 +0300

    Update CAH.js

--- CAH.js
+++ CAH.js
@@ -1,3 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+const port = process.env.PORT || 4000
+
 class Game {
 
   GAME_CHATBOT_STATE_DICT = {}
